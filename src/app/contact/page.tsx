995c11af51a91d24817ff1853e1d8014dbee58eb
donjne <davidjrn247@gmail.com>
2025-01-04 13:41:33 +0100

    fixingg

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,6 @@
 "use client"
-import React from 'react';
 import ContactPage from '@/components/Contact/Form';
 
 export default function Signup() {
-  return (
-  <main>
-    <ContactPage />
-  </main>
-  );
+  return ContactPage
 }
