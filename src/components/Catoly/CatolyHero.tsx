4d83ed2d6a6174c79f73aa23fc69134331eff1cd
donjne <davidjrn247@gmail.com>
2025-01-18 05:49:04 +0100

    h29

--- src/components/Catoly/CatolyHero.tsx
+++ src/components/Catoly/CatolyHero.tsx
@@ -51,17 +51,16 @@ const CatolyHero = () => {
         </motion.div>
 
         {/* Right Image */}
-        <motion.div 
-          className="relative w-full lg:w-[500px] h-[500px]"
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-        >
-          <div>
-            {/* Green Circle Background */}
-            <AnimatedSphere />
-          </div>
-        </motion.div>
+            <motion.div 
+            className="relative w-full lg:w-[500px] h-[500px] flex items-center justify-center"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            >
+            <div className="w-full h-full">
+                <AnimatedSphere />
+            </div>
+            </motion.div>
       </div>
     </div>
   );
